2d41aed14f2560d2a6770896b25aede3674d3952
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-08-02 15:11:48 +0300

    finish feedback app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,35 @@ class App extends Component {
   };
 
   onLeaveFeedback = event => {
-    this.setState(prevstate =>
-      event.target.className === 'good'
-        ? { good: prevstate.good + 1 }
-        : event.target.className === 'neutral' ? { neutral: prevstate.neutral + 1 } : { bad: prevstate.bad + 1 }
+    this.setState(
+      prevstate =>
+        event.target.className === 'good'
+          ? { good: prevstate.good + 1 }
+          : event.target.className === 'neutral'
+          ? { neutral: prevstate.neutral + 1 }
+          : { bad: prevstate.bad + 1 },
+      this.countTotalFeedback,
       );
   };
-  // countPositiveFeedbackPercentage();
+
+  countTotalFeedback = () => {
+    this.setState(
+      {
+        total: this.state.good + this.state.neutral + this.state.bad,
+      },
+      this.countPositiveFeedbackPercentage
+    );
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    this.setState({
+      percentage:
+        Math.round((
+          this.state.good /
+            (this.state.good + this.state.neutral + this.state.bad)
+        ) * 100),
+    });
+  };
 
   render() {
     return (
@@ -35,20 +57,11 @@ class App extends Component {
           Bad
         </button>
         <h3>Statistics</h3>
-        <ul>
-          <li>Good: {this.state.good}</li>
-          <li>Neutral: {this.state.neutral}</li>
-          <li>Bad: {this.state.bad}</li>
-          <li>
-            Total: {this.state.good + this.state.neutral + this.state.bad}
-          </li>
-          <li>
-            Positive feedback: {(this.state.good /
-              (this.state.good + this.state.neutral + this.state.bad)) *
-              100}
-            %
-          </li>
-        </ul>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total: {this.state.total}</p>
+        <p>Positive feedback: {this.state.percentage}%</p>
       </div>
     );
   }
